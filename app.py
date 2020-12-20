d602a3c37ef2378ec763f68c3041ca0593c91c9d
Uğur Keskin <ugurkkkkkk@hotmail.com>
2020-12-20 15:03:30 +0300

    Added Hiearchy

--- app.py
+++ app.py
@@ -8,24 +8,6 @@ except:
     import pickle
 import pandas as pd
 from flasgger import Swagger
-def load_models():
-    models = []
-    for modelName in os.listdir("models"):
-        models.append(pickle.load(open(modelName,"rb",-1)))
-    return models
-
-
-def load_dataset():
-    col_list = ["author","text"]
-    return pd.read_csv("dataset/train.csv",usecols=col_list),pd.read_csv("dataset/test.csv",usecols=col_list)
-
-
-models = []
-models = load_models() #Modelleri localden çekecek
-train,test = load_dataset()
-
-app = Flask(__name__)
-swagger = Swagger(app)
 
 
 Model_dict = {
@@ -39,6 +21,31 @@ Model_dict = {
         "TF-IDF": None,
     }
 }
+
+def load_models():
+    models = []
+    try:
+        for mainModel in list(Model_dict.keys()):
+            for subModel in list(Model_dict[mainModel].keys()):
+                Model_dict[mainModel][subModel] =pickle.load(open("models/"+mainModel+"/"+subModel+".pkl","rb",-1))
+    except: 
+        pass
+    return models
+
+
+def load_dataset():
+    col_list = ["author","text"]
+    return pd.read_csv("dataset/train.csv",usecols=col_list),pd.read_csv("dataset/test.csv",usecols=col_list)
+
+
+models = load_models() #Modelleri localden çekecek
+train_data,test_data = load_dataset()
+
+app = Flask(__name__)
+swagger = Swagger(app)
+
+
+
 
 
 def getModelFromDict(args):
@@ -61,38 +68,53 @@ def getModelFromDict(args):
     return requestedModel, last_args
 
 
+def runMethodOfModel(methodName,args,material):
+    results = []
+    if(args[-1]=="ALL"):
+        for key in list(requestedModel.keys()):
+            results.append(Model_dict[key].getattr(model,methodName)(material,[]))
+    else:
+        model, last_args =  getModelFromDict(args)
+        results.append(getattr(model,methodName)(material,last_args))
+
+    return results
+
+
 
 @app.route('/')
 def hello():
     return render_template("index.html");
 
 
+#Modele tahmin yaptırır
+
 @app.route('/predict',methods= ["POST"])
-def predict(): #Model.NGRAM şeklinde bir parametre
-    predictions = []
+def predict():
     parameters = request.get_json()
-    
     text = parameters.get("text")
     args = parameters.get("args")
-    print(args)
-    if(args[-1]=="ALL"):
-        for i in range(0,3):
-            predictions.append(models[i].predict(text))
-    else:
-        model, last_args =  getModelFromDict(args)
-        predictions.append(model.predict(text,last_args))
 
-    return predictions
+    return runMethodOfModel("predict",args,text)
+
+
 
 #Mevcut modeli eğitir ve train accuracy'sini döndürür
+
 @app.route('/train')
-def train(modelEnum,**args): 
-    return models[modelEnum].train(train,args)
+def train(): 
+    parameters = request.get_json()
+    args = parameters.get("args")
+    return runMethodOfModel("train",args,test_data)
+
+
 
 #Mevcut modeli test eder ve test accuracy'sini döndürür
+
 @app.route('/test')
-def test(modelEnum,**args): 
-    return models[modelEnum].test(test,args)
+def test(): 
+    parameters = request.get_json()
+    args = parameters.get("args")
+    return runMethodOfModel("test",args,train_data)
 
 
 if __name__ == '__main__':
